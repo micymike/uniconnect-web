70abb05ef75cb0a65980999ccc3a155e9472e461
micymike <mosesmmichael878@gmail.com>
2025-07-23 22:24:06 +0300

    Update allowedHosts in Vite config to fix domain formatting
    Correct domain formatting for 'uniconnect-web-fsm9.onrender.com' in allowedHosts array and add non-https version to ensure proper server access.

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,6 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['www.uniconnect.store', 'https://uniconnect-web-fsm9.onrender.com.']
+    allowedHosts: ['www.uniconnect.store', 'https://uniconnect-web-fsm9.onrender.com', 'uniconnect-web-fsm9.onrender.com'],
   }
 })
